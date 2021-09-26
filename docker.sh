f2a495192e374f98ced84530054dcd37d68cf78e
Aleksandra Mozdyniewicz <mozdyniewicz.a@gmail.com>
2021-09-26 22:22:58 +0200

    update po feedbacku

--- docker.sh
+++ docker.sh
@@ -1,11 +1,18 @@
+IMAGES=('ubuntu' 'mongo' 'redis')
+
+for image in "${IMAGES[@]}"; do
+    docker manifest inspect -v $image >> logs.txt
+done
+
+
 docker pull ubuntu
 docker pull mongodb
 docker pull redis
 
 
 docker run -d --name linuch -it ubuntu
-docker run -d --name db -p 8080:80 -it mongo
-docker run -d --name memcache -p 8080:80 -it redis
+docker run -d --name db -p 27017:27017 -it mongo
+docker run -d --name memcache -p 8080:8080 -it redis
 
 docker container stop memcache
 docker container stop db
